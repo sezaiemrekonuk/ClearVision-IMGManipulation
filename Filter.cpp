8d3f9b7e2966c0203a6d04440c8c1b85966d3260
sezaiemrekonuk <sezaikonuk@hacettepe.edu.tr>
2024-10-19 16:59:12 +0300

    odev

--- Filter.cpp
+++ Filter.cpp
@@ -5,27 +5,166 @@
 #include <numeric>
 #include <math.h>
 
+float gaussian_2d_weight_finder(int x, int y, double sigma)
+{
+    return (1.0 / (2 * M_PI * sigma * sigma)) * exp(-(x * x + y * y) / (2 * sigma * sigma));
+}
+
 // Mean Filter
-void Filter::apply_mean_filter(GrayscaleImage& image, int kernelSize) {
+void Filter::apply_mean_filter(GrayscaleImage &image, int kernelSize)
+{
     // TODO: Your code goes here.
     // 1. Copy the original image for reference.
+    GrayscaleImage copyImage = image;
     // 2. For each pixel, calculate the mean value of its neighbors using a kernel.
-    // 3. Update each pixel with the computed mean.
+    for (int i = 0; i < image.get_height(); i++)
+    {
+        for (int j = 0; j < image.get_width(); j++)
+        {
+            // creating a vector to store the pixel values of the neighbours
+            // i am using a vector because the number of neighbours can differ from place to place (for example edges).
+            std::vector<int> neighbours;
+
+            // iterating over the image using kernel size.
+
+            // notes bcz im a little bit stupid
+            // i - kernelSize / 2 plus j - kernelSize / 2 == top left
+            // i + kernelSize / 2 plus j + kernelSize / 2 == bottom right
+            // end of notes
+            for (int a = i - kernelSize / 2; a <= i + kernelSize / 2; a++)
+            {
+                for (int b = j - kernelSize / 2; b <= j + kernelSize / 2; b++)
+                {
+                    // checking if the pixel is inside the image, if so add the pixel value to vector.
+                    if (a >= 0 && a < image.get_height() && b >= 0 && b < image.get_width())
+                    {
+                        neighbours.push_back(copyImage.get_pixel(a, b));
+                    }
+                }
+            }
+
+            // mean
+            // using accumulate (i learned it from stackoverflow xD its a cool function)
+            int mean = std::accumulate(neighbours.begin(), neighbours.end(), 0) / neighbours.size(); // its a c++3 function and might not work will try later.
+
+            // 3. Update each pixel with the computed mean.
+            // copyImage.set_pixel(i, j, mean); // wrong image
+            image.set_pixel(i, j, mean);
+        }
+    }
 }
 
 // Gaussian Smoothing Filter
-void Filter::apply_gaussian_smoothing(GrayscaleImage& image, int kernelSize, double sigma) {
+void Filter::apply_gaussian_smoothing(GrayscaleImage &image, int kernelSize, double sigma)
+{
     // TODO: Your code goes here.
     // 1. Create a Gaussian kernel based on the given sigma value.
     // 2. Normalize the kernel to ensure it sums to 1.
     // 3. For each pixel, compute the weighted sum using the kernel.
     // 4. Update the pixel values with the smoothed results.
+
+    GrayscaleImage copyImage = image;
+
+    std::vector<std::vector<float>> kernel(kernelSize, std::vector<float>(kernelSize)); // should be a 2d vector to store the weights of the kernel according to the distance from the center.
+
+    // calculating the weights of the kernel
+    for (int i = 0; i < kernelSize; i++)
+    {
+        for (int j = 0; j < kernelSize; j++)
+        {
+            kernel[i][j] = gaussian_2d_weight_finder(i - kernelSize / 2, j - kernelSize / 2, sigma);
+        }
+    }
+
+    // accumulate doesn't work??
+    unsigned int sum = 0;
+    for (int i = 0; i < kernelSize; i++)
+    {
+        for (int j = 0; j < kernelSize; j++)
+        {
+            sum += kernel[i][j];
+        }
+    }
+
+    // normalizing the kernel
+    for (int i = 0; i < kernelSize; i++)
+    {
+        for (int j = 0; j < kernelSize; j++)
+        {
+            kernel[i][j] /= sum;
+        }
+    }
+
+    // applying the filter
+    for (int i = 0; i < image.get_height(); i++)
+    {
+        for (int j = 0; j < image.get_width(); j++)
+        {
+            int **neighbours = new int *[kernelSize];
+            for (int a = 0; a < kernelSize; a++)
+            {
+                neighbours[a] = new int[kernelSize];
+            }
+
+            // setting the values to neighbours array
+            for (int a = i - kernelSize / 2; a <= i + kernelSize / 2; a++)
+            {
+                for (int b = j - kernelSize / 2; b <= j + kernelSize / 2; b++)
+                {
+                    // if a and b are inside the image seting the pixel value to the neighbours matrix
+                    if (a >= 0 && a < image.get_height() && b >= 0 && b < image.get_width())
+                    {
+                        neighbours[a - i + kernelSize / 2][b - j + kernelSize / 2] = copyImage.get_pixel(a, b);
+                    }
+                    else
+                    {
+                        // if a and b are outside the image set the pixel value to 0
+                        neighbours[a - i + kernelSize / 2][b - j + kernelSize / 2] = 0;
+                    }
+                }
+            }
+
+            // calculating the weighted sum
+            int weightedSum = 0;
+            for (int a = 0; a < kernelSize; a++)
+            {
+                for (int b = 0; b < kernelSize; b++)
+                {
+                    weightedSum += neighbours[a][b] * kernel[a][b];
+                }
+            }
+
+            // updating the current images pixel value
+            image.set_pixel(i, j, (int)weightedSum);
+        }
+    }
 }
 
 // Unsharp Masking Filter
-void Filter::apply_unsharp_mask(GrayscaleImage& image, int kernelSize, double amount) {
+void Filter::apply_unsharp_mask(GrayscaleImage &image, int kernelSize, double amount)
+{
     // TODO: Your code goes here.
     // 1. Blur the image using Gaussian smoothing, use the default sigma given in the header.
+    GrayscaleImage copyImage = image;
+    Filter::apply_gaussian_smoothing(copyImage, kernelSize, 1.0);
     // 2. For each pixel, apply the unsharp mask formula: original + amount * (original - blurred).
-    // 3. Clip values to ensure they are within a valid range [0-255].
+    for (int i = 0; i < image.get_height(); i++)
+    {
+        for (int j = 0; j < image.get_width(); j++)
+        {
+            int original = image.get_pixel(i, j);
+            int blurred = copyImage.get_pixel(i, j);
+            int unsharpMask = original + amount * (original - blurred);
+            // 3. Clip values to ensure they are within a valid range [0-255].
+            if (unsharpMask < 0)
+            {
+                unsharpMask = 0;
+            }
+            else if (unsharpMask > 255)
+            {
+                unsharpMask = 255;
+            }
+            image.set_pixel(i, j, unsharpMask);
+        }
+    }
 }
--- SecretImage.cpp
+++ SecretImage.cpp
@@ -1,85 +1,244 @@
 #include "SecretImage.h"
-
+#include "fstream"
 
 // Constructor: split image into upper and lower triangular arrays
-SecretImage::SecretImage(const GrayscaleImage& image) {
+SecretImage::SecretImage(const GrayscaleImage &image)
+{
     // TODO: Your code goes here.
     // 1. Dynamically allocate the memory for the upper and lower triangular matrices.
+    // int upper_size;
+    // int lower_size;
+
+    // for (int i = 0; i < image.get_height(); i++){
+    //     for (int j = i; j < image.get_width(); j++){
+    //         upper_size++;
+    //     }
+    // }
+
+    // for (int i = 0; i < image.get_height(); i++){
+    //     for (int j = 0; j < i; j++){
+    //         lower_size++;
+    //     }
+    // }
+
+    // upper_triangular = new int[upper_size];
+    // lower_triangular = new int[lower_size];
+
+    // it has higher space complexity but easier to access, so i chose this.
+    upper_triangular = new int[image.get_width() * image.get_height()];
+    lower_triangular = new int[image.get_width() * image.get_height()];
+
     // 2. Fill both matrices with the pixels from the GrayscaleImage.
+    // upper part:
+    for (int i = 0; i < image.get_height(); i++)
+    {
+        for (int j = i; j < image.get_width(); j++)
+        {
+            upper_triangular[i * image.get_width() + j] = image.get_pixel(j, i);
+        }
+    }
+
+    // lower part:
+    for (int i = 0; i < image.get_height(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            lower_triangular[i * image.get_width() + j] = image.get_pixel(j, i);
+        }
+    }
 }
 
 // Constructor: instantiate based on data read from file
-SecretImage::SecretImage(int w, int h, int * upper, int * lower) {
+SecretImage::SecretImage(int w, int h, int *upper, int *lower)
+{
     // TODO: Your code goes here.
     // Since file reading part should dynamically allocate upper and lower matrices.
     // You should simply copy the parameters to instance variables.
+    width = w;
+    height = h;
+    upper_triangular = upper;
+    lower_triangular = lower;
 }
 
-
 // Destructor: free the arrays
-SecretImage::~SecretImage() {
+SecretImage::~SecretImage()
+{
     // TODO: Your code goes here.
     // Simply free the dynamically allocated memory
     // for the upper and lower triangular matrices.
+    delete[] upper_triangular;
+    delete[] lower_triangular;
 }
 
 // Reconstructs and returns the full image from upper and lower triangular matrices.
-GrayscaleImage SecretImage::reconstruct() const {
+GrayscaleImage SecretImage::reconstruct() const
+{
     GrayscaleImage image(width, height);
 
     // TODO: Your code goes here.
+    // fitting the upper part
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = i; j < width; j++)
+        {
+            image.set_pixel(j, i, upper_triangular[(width * i) + j]);
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            image.set_pixel(j, i, lower_triangular[(width * i) + j]);
+        }
+    }
 
     return image;
 }
 
 // Save the filtered image back to the triangular arrays
-void SecretImage::save_back(const GrayscaleImage& image) {
+void SecretImage::save_back(const GrayscaleImage &image)
+{
     // TODO: Your code goes here.
-    // Update the lower and upper triangular matrices 
+    // Update the lower and upper triangular matrices
     // based on the GrayscaleImage given as the parameter.
+    width = image.get_width();
+    height = image.get_height();
+
+    // upper part
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = i; j < width; j++)
+        {
+            upper_triangular[(width * i) + j] = image.get_pixel(j, i);
+        }
+    }
+
+    // lower part
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            lower_triangular[(width * i) + j] = image.get_pixel(j, i);
+        }
+    }
 }
 
 // Save the upper and lower triangular arrays to a file
-void SecretImage::save_to_file(const std::string& filename) {
+void SecretImage::save_to_file(const std::string &filename)
+{
     // TODO: Your code goes here.
+    std::ofstream file(filename);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Error: File could not be opened." << std::endl;
+        return;
+    }
     // 1. Write width and height on the first line, separated by a single space.
+    file << width << " " << height << std::endl;
     // 2. Write the upper_triangular array to the second line.
-    // Ensure that the elements are space-separated. 
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = i; j < width; j++)
+        {
+            file << upper_triangular[(width * i) + j] << " ";
+        }
+    }
+    file << std::endl;
+
+    // Ensure that the elements are space-separated.
     // If there are 15 elements, write them as: "element1 element2 ... element15"
     // 3. Write the lower_triangular array to the third line in a similar manner
     // as the second line.
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            file << lower_triangular[(width * i) + j] << " ";
+        }
+    }
 }
 
 // Static function to load a SecretImage from a file
-SecretImage SecretImage::load_from_file(const std::string& filename) {
+SecretImage SecretImage::load_from_file(const std::string &filename)
+{
     // TODO: Your code goes here.
     // 1. Open the file and read width and height from the first line, separated by a space.
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: File could not be opened." << std::endl;
+        return SecretImage(nullptr);
+    }
+
+    // getting the line
+    std::string line;
+    std::getline(file, line);
+
+    // getting width and height from the line, first converting it to a string stream
+    std::istringstream iss(line);
+
+    int w, h;
+    iss >> w >> h;
     // 2. Calculate the sizes of the upper and lower triangular arrays.
+    int upper_size = w * h, lower_size = w * h;
+
     // 3. Allocate memory for both arrays.
+    int *upper = new int[upper_size];
+    int *lower = new int[lower_size];
+
     // 4. Read the upper_triangular array from the second line, space-separated.
+    std::getline(file, line);
+    std::istringstream issut(line);
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = i; j < w; j++)
+        {
+            iss >> upper[(w * i) + j];
+        }
+    }
+
+    std::getline(file, line);
+    std::istringstream isslt(line);
+
     // 5. Read the lower_triangular array from the third line, space-separated.
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            iss >> lower[(w * i) + j];
+        }
+    }
     // 6. Close the file and return a SecretImage object initialized with the
+    file.close();
+
     //    width, height, and triangular arrays.
-    SecretImage secret_image(nullptr);
+    SecretImage secret_image(w, h, upper, lower);
     return secret_image;
 }
 
 // Returns a pointer to the upper triangular part of the secret image.
-int * SecretImage::get_upper_triangular() const {
+int *SecretImage::get_upper_triangular() const
+{
     return upper_triangular;
 }
 
 // Returns a pointer to the lower triangular part of the secret image.
-int * SecretImage::get_lower_triangular() const {
+int *SecretImage::get_lower_triangular() const
+{
     return lower_triangular;
 }
 
 // Returns the width of the secret image.
-int SecretImage::get_width() const {
+int SecretImage::get_width() const
+{
     return width;
 }
 
 // Returns the height of the secret image.
-int SecretImage::get_height() const {
+int SecretImage::get_height() const
+{
     return height;
 }
